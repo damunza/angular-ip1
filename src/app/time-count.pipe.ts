3eb3d3565f5e9fb34919712653b88e76086042ae
damunza <danidamunza@gmail.com>
2018-08-10 17:52:53 +0300

    added a timer

---
+++ src/app/time-count.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timeCount'
+})
+export class TimeCountPipe implements PipeTransform {
+
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    var dateDifference =Math.abs(value-todayWithNoTime )//added a +  instead of a -
+    const secondsInHour=3600;
+    var dateDifferenceSeconds=dateDifference*0.001;
+    var dateCounter = dateDifferenceSeconds/secondsInHour;
+
+    return dateCounter;
+
+  }
+
+}
