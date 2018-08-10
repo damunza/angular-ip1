e7470efda1b676ee8ae59c5cbc262007396a6766
damunza <danidamunza@gmail.com>
2018-08-10 17:14:39 +0300

    added a date entry section

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,7 @@ export class Quote {
 
 public showAuthor:boolean;
 
-constructor(public id:number, public quote:string, public author:string, public submitter:string){
+constructor(public id:number, public quote:string, public author:string, public submitter:string, public submitTime:Date){
 
 this.showAuthor=false;
 
