df5f3bc88180ba2c6714938f5c95bd41ec9dc4d8
damunza <danidamunza@gmail.com>
2018-08-13 10:27:38 +0300

    definned the upvote and downvote properties to be 0

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -5,7 +5,8 @@ public showAuthor:boolean;
 constructor(public id:number, public quote:string, public author:string, public submitter:string, public submitTime:Date, public upvote:number, public downvote:number){
 
 this.showAuthor=false;
-
+this.downvote=0;
+this.upvote=0;
 }
 
 }
